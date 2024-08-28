53641d6051f9ab13bb26f7c9c2f8c9070f5683f6
João Pedro Erhardt <joaopedro.erhardt@gmail.com>
2024-08-28 15:38:02 -0300

    add resolucao questao 1 - ListaExercicio3

---
+++ src/ListaExercicio3/Questao1/App.java
@@ -0,0 +1,24 @@
+package ListaExercicio3.Questao1;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        double totalImposto = 0;
+
+        Funcionario funcionario = criarConta();
+        totalImposto = funcionario.calcularIrpf();
+        JOptionPane.showMessageDialog(null, "O imposto total a ser pago é: R$ " + totalImposto);
+    }
+
+    static Funcionario criarConta()
+    {
+        String nome = JOptionPane.showInputDialog(null, "Informe o nome do funcionario");
+        double salario = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o salario"));
+
+        return new Funcionario(nome, salario);
+    }
+}
--- src/ListaExercicio3/Questao1/Funcionario.java
+++ src/ListaExercicio3/Questao1/Funcionario.java
@@ -4,23 +4,87 @@ public class Funcionario {
     private String nome;
     private double salario;
 
-    public String getNome() {
+    public Funcionario(String nome, double salario)
+    {
+        setNome(nome);
+        setSalario(salario);
+    }
+
+    public String getNome()
+    {
         return nome;
     }
 
-    public void setNome(String nome) throws IllegalArgumentException {
+    public void setNome(String nome) throws IllegalArgumentException
+    {
         if (nome.trim().isEmpty()) {
             throw new IllegalArgumentException("O nome é inválido");
         }
         this.nome = nome;
     }
 
-    public double getSalario() {
+    public double getSalario()
+    {
         return salario;
     }
 
-    public void setSalario(double salario) {
+    public void setSalario(double salario)
+    {
         this.salario = salario;
+    }
+
+    public double calcularIrpf() {
+        double diferenca = 0;
+        double total = 0;
+
+        // faixa 1
+        if (salario <= 1903.98) {
+            return 0;
+        }
+
+        // faixa 2
+        if (salario >= 1903.99) {
+            if(salario < 2826.65) {
+                diferenca = salario - 1903.99;
+                total += (diferenca * 0.075);
+            }
+            else {
+                diferenca = 2826.65 - 1903.99;
+                total += (diferenca * 0.075);
+            }
+        }
+
+        // faixa 3
+        if (salario >= 2826.66) {
+            if(salario < 3751.05) {
+                diferenca = salario - 2826.66;
+                total += (diferenca * 0.15);
+            }
+            else {
+                diferenca = 3751.05 - 2826.66;
+                total += (diferenca * 0.15);
+            }
+        }
+
+        // faixa 4
+        if (salario >= 3751.06) {
+            if(salario < 4664.68) {
+                diferenca = salario - 3751.06;
+                total += (diferenca * 0.225);
+            }
+            else {
+                diferenca = 4664.68 - 3751.06;
+                total += (diferenca * 0.225);
+            }
+        }
+
+        // faixa 5
+        if (salario >= 4664.69) {
+            diferenca = salario - 4664.69;
+            total += (diferenca * 0.275);
+        }
+
+        return total;
     }
 
     /*
